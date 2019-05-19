bc18be9729c12c54c73e3056808d33f488fd41f3
todoesverso <todoesverso@gmail.com>
2019-05-19 18:28:58 -0300

    Simple and stupid mono amp (pure copy and paste)

---
+++ todoes-amp.lv2/test/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+TMP_FOLDER=$(mktemp -d)
+NAME_IN=test
+NAME_OUT=test_out
+URI=https://github.com/todoesverso/lv2s/todoes-amp
+LV2FILE=/home/todoesverso/repos/lv2file/lv2file
+
+
+# Create files
+FILE_IN=${TMP_FOLDER}/${NAME_IN}.wav
+FILE_OUT=${TMP_FOLDER}/${NAME_OUT}.wav
+sox -V -r 48000 -n -b 16 ${FILE_IN} synth 0.1 sin 100 vol -3dB
+sox -V ${FILE_IN} -n stats
+
+# Process
+LV2_PATH=$(pwd)/plugin ${LV2FILE} -i ${FILE_IN} -o ${FILE_OUT} -c1:in -pgain:3 ${URI}
+sox ${FILE_IN} -n stats
+sox ${FILE_OUT} -n stats
+
+audacity ${FILE_IN} ${FILE_OUT}
+
+# Clean 
+rm -fr ${TMP_FOLDER}
